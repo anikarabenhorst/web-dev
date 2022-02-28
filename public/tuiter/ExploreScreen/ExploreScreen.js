993841078e079cdd32298f9e2e9b8301c57ee415
anikarabenhorst <anikacchase@gmail.com>
2022-02-28 11:06:52 -0500

    a5 javascript sp22

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -1,6 +1,6 @@
-import NavigationSidebar from "../NavigationSidebar/index.js";
-// import ExploreComponent from "./ExploreComponent.js";
-import WhoToFollowList from "../WhoToFollowList/index.js";
+import NavigationSidebar from "../NavigationSidebar";
+import ExploreComponent from "./ExploreComponent.js";
+import WhoToFollowList from "../WhoToFollowList";
 
 (function ($) {
     $('#wd-explore').append(`
@@ -9,6 +9,7 @@ import WhoToFollowList from "../WhoToFollowList/index.js";
             ${NavigationSidebar()}
         </div>
         <div class="col-10 col-md-10 col-lg-7 col-xl-6">
+          ${ExploreComponent()}
         </div>
         <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
           ${WhoToFollowList()}
