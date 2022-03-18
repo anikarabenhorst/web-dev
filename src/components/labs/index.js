3ec9c5c26197053ce9a45f6e72113fc114d4131f
anikarabenhorst <anikacchase@gmail.com>
2022-03-18 12:07:48 -0400

    nav and who to follow

--- src/components/labs/index.js
+++ src/components/labs/index.js
@@ -3,12 +3,19 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput from "./conditional-output";
 import TodoList from "./todo/todo-list";
+import {Link} from "react-router-dom";
 
 
 const Labs = () => {
     return(
         <div>
             <h1>Labs</h1>
+            <Link to="/hello">
+                Hello World
+            </Link> |
+            <Link to="/tuiter">
+                Tuiter
+            </Link>
             <TodoList/>
             <ConditionalOutput/>
             <Styles/>
--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import NavigationSidebar from "./NavigationSidebar";
+import WhoToFollowList from "./WhoToFollowList";
 
 const Tuiter = () => {
     return(
         <>
-            <h1>Tuiter</h1>
-            <Link to="/hello">
-                Hello
-            </Link> |
-            <Link to="/">
-                Labs
-            </Link>
+            <NavigationSidebar active="home"/>
+            <WhoToFollowList/>
         </>
 
-
-)
+    )
 };
 
 export default Tuiter;
