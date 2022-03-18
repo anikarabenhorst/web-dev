b97b59f27c34c67afe06669afdb887a33399c5b5
anikarabenhorst <anikacchase@gmail.com>
2022-03-18 11:12:35 -0400

    a6 lab

---
+++ src/components/labs/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
+import TodoList from "./todo/todo-list";
+
+
+const Labs = () => {
+    return(
+        <div>
+            <h1>Labs</h1>
+            <TodoList/>
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
+        </div>
+    )
+};
+
+export default Labs;
