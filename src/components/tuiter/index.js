5941b8ad263139f4d1c1c6b8ff9d0a4a618372c0
anikarabenhorst <anikacchase@gmail.com>
2022-03-19 20:42:44 -0400

    postsummarylist

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import NavigationSidebar from "./NavigationSidebar";
 import WhoToFollowList from "./WhoToFollowList";
+import PostSummaryList from "./PostSummaryList";
 
 const Tuiter = () => {
     return(
         <>
             <NavigationSidebar active="home"/>
+            <PostSummaryList/>
             <WhoToFollowList/>
         </>
-
     )
 };
 
